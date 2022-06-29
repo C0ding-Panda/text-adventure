494f878c8a938040bd5a10862e0b360ff965f2eb
Panda <106486268+C0ding-Panda@users.noreply.github.com>
2022-06-29 18:57:52 +0200

    scroll nach oben, letzte parts adden!, v1.5 work

--- docs/script.js
+++ docs/script.js
@@ -20,9 +20,9 @@ function start() {
     document.getElementById("einfuerung_id").style.display = "none";
     document.getElementById("chapter1").style.display = "block";
     document.getElementById("btns_con_unten1_id").style.display = "flex";
-
-}//alles starten
-function stop() {
+    window.scrollBy({ top: 700, left: 0, behavior: "smooth" });
+}
+    function stop() {
     document.getElementById("einfuerung_id").style.display = "block";//kapitel 1
     document.getElementById("chapter1").style.display = "none";//kapitel 2
     document.getElementById("btns_con_unten1_id").style.display = "none";//kapitel 2
@@ -50,30 +50,35 @@ function bootkauf() {
     document.getElementById("btns_con_unten1_id").style.display = "none";
     document.getElementById("chapter_bootkauf").style.display = "block";
     document.getElementById("btns_con_unten2_id").style.display = "flex";
+    window.scrollBy({ top: -200, left: 0, behavior: "smooth" });//nach oben scrollen
 }//boot kaufen
 function essengehen() {
     document.getElementById("chapter1").style.display = "none";
     document.getElementById("btns_con_unten1_id").style.display = "none";
     document.getElementById("chapter_essen").style.display = "block";
     document.getElementById("sensenmann_con_ch4_id").style.display = "flex";
+    window.scrollBy({ top: -450, left: 0,});//nach oben scrollen
 }//essen gehen
 function karte_folgen() {
     document.getElementById("chapter_bootkauf").style.display = "none";
     document.getElementById("btns_con_unten2_id").style.display = "none";
     document.getElementById("chapter_karte_befolgen").style.display = "block";
     document.getElementById("btns_con_unten4_id").style.display = "flex";
+    window.scrollBy({ top: -400, left: 0, behavior: "smooth" });//nach oben scrollen
 }//wenn man der Karte folgt
 function karte_nichtfolgen() {
     document.getElementById("chapter_bootkauf").style.display = "none";
     document.getElementById("btns_con_unten2_id").style.display = "none";
     document.getElementById("chapter_karte_nichtbefolgen").style.display = "block";
     document.getElementById("sensenmann_con_ch6_id").style.display = "flex";
+    window.scrollBy({ top: -500, left: 0,});//nach oben scrollen
 }//wenn man der Karte nicht folgt
 function verkaufen() {
     document.getElementById("chapter_bootkauf").style.display = "none";
     document.getElementById("btns_con_unten2_id").style.display = "none";
     document.getElementById("chapter_karte_verkaufen").style.display = "block";
     document.getElementById("sensenmann_con_ch7_id").style.display = "flex";
+    window.scrollBy({ top: -550, left: 0,});//nach oben scrollen
 }//wenn man sein BOot verkaufen will
 function wald_gehen() {
     document.getElementById("chapter_karte_befolgen").style.display = "none";
